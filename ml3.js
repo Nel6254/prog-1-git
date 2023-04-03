bd58f85330164e4d18b2d888cef0e1d9132a5e36
Nel6254 <ralfs3213@gmail.com>
2023-04-03 21:54:04 +0100

    button and image change (failed)

--- ml3.js
+++ ml3.js
@@ -1,1 +1,14 @@
- 
+ const images = document.querySelectorAll('.js-image')
+ const next = document.querySelector('.js-next')
+ const prev = document.querySelector('.js-prev')
+ let activeIndex = 0
+
+ next.addEventListener('click', () => {
+    images.forEach((image) => {
+        image.classList.remove('active')
+    })
+
+    activeIndex = activeIndex + 1
+
+    image[activeIndex].classList.add('active')
+ })
