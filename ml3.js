eecedc385b64685b10721f4088e6b20549e0e8df
Nel6254 <ralfs3213@gmail.com>
2023-04-04 19:43:10 +0100

    new change

--- ml3.js
+++ ml3.js
@@ -3,12 +3,32 @@
  const prev = document.querySelector('.js-prev')
  let activeIndex = 0
 
- next.addEventListener('click', () => {
-    images.forEach((image) => {
-        image.classList.remove('active')
+const removeActiveFromAllImages =() => {
+    images.forEach((images) => {
+        images.classList.remove('active')
     })
+}
 
-    activeIndex = activeIndex + 1
+ next.addEventListener('click', () => {
+    removeActiveFromAllImages()
+
+    if(activeIndex === images.length - 1) {
+        activeIndex = 0
+    } else {
+        activeIndex = activeIndex + 1
+    }
+
+    image[activeIndex].classList.add('active')
+ })
+
+ prev.addEventListener('click', () => {
+    removeActiveFromAllImages()
+
+    if(activeIndex === 0) {
+        activeIndex = images.length - 1
+    } else {
+        activeIndex = activeIndex -  1
+    }
 
     image[activeIndex].classList.add('active')
  })
