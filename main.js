a75e2056c8241e2f653d048eb4734de9439f9923
Nel6254 <ralfs3213@gmail.com>
2024-05-22 18:46:21 +0300

    update

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ $.getJSON("vielas.json", function(json){
         $("#KT").append(table_row);
     }    
      });
+     fetch("vielas.json")
+.then(function(response){
+	return response.json();
+})
+.then(function(products){
+	let placeholder = document.querySelector("#data-output");
+	let out = "";
+	for(let product of products){
+		out += `
+			<tr>
+				<td>${product.id}</td>
+				<td>${product.nosaukums}</td>
+				<td>${product.tips}</td>
+				<td>${product.apakstips}</td>
+				<td>${product.skaits}</td>
+                <td>${product.daudzums}</td>
+                <td>${product.komentari}</td>
+			</tr>
+		`;
+	}
+
+	placeholder.innerHTML = out;
+});
--- main3.js
+++ main3.js
@@ -1,11 +1,22 @@
-fetch("inventars.json")
-.then(function(response){
-	return response.json();
-})
-.then(function(products){
-	let placeholder = document.querySelector("#data-output");
-	let out = "";
-	for(let product of products){
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", updateButton);
+
+function updateButton() {
+
+var choice = file.value;
+
+switch (choice) {
+  case "2":
+	fetch("vielas.json")
+		.then(function(response){
+			return response.json();
+	})
+	.then(function(products){
+		let placeholder = document.querySelector("#data-output");
+		let out = "";
+		for(let product of products){
 		out += `
 			<tr>
 				<td>${product.id}</td>
@@ -20,4 +31,56 @@ fetch("inventars.json")
 	}
 
 	placeholder.innerHTML = out;
-});
+	});
+	break;
+	case "3":
+		fetch("inventars.json")
+		.then(function(response){
+			return response.json();
+	})
+	.then(function(products){
+		let placeholder = document.querySelector("#data-output");
+		let out = "";
+		for(let product of products){
+		out += `
+			<tr>
+				<td>${product.id}</td>
+				<td>${product.nosaukums}</td>
+				<td>${product.tips}</td>
+				<td>${product.apakstips}</td>
+				<td>${product.skaits}</td>
+                <td>${product.daudzums}</td>
+                <td>${product.komentari}</td>
+			</tr>
+		`;
+	}
+
+	placeholder.innerHTML = out;
+	});
+	break;
+
+	default:
+		fetch("inventars.json")
+		.then(function(response){
+			return response.json();
+	})
+	.then(function(products){
+		let placeholder = document.querySelector("#data-output");
+		let out = "";
+		for(let product of products){
+		out += `
+			<tr>
+				<td>${product.id}</td>
+				<td>${product.nosaukums}</td>
+				<td>${product.tips}</td>
+				<td>${product.apakstips}</td>
+				<td>${product.skaits}</td>
+                <td>${product.daudzums}</td>
+                <td>${product.komentari}</td>
+			</tr>
+		`;
+	}
+	placeholder.innerHTML = out;
+	});
+	}
+}
