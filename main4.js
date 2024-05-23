3285242e90ff3642f588dd8f18ea72f233600038
Nel6254 <ralfs3213@gmail.com>
2024-05-23 23:54:15 +0300

    update

--- main4.js
+++ main4.js
@@ -1,4 +1,16 @@
-fetch("inventars.json")
+const buttons = document.getElementsByClassName("button4");
+
+buttons.addEventListener("click", () => {
+
+    const choise = buttons.value;
+    saraksti(choise);
+});
+
+function saraksti(choise) {
+    choise = choise.replace(" ", "").toLowerCase();
+    const url = `${choise}.json`;
+
+fetch(url)
 .then(function(response){
     return response.json();
 })
@@ -19,30 +31,4 @@ out += `
 `;
 }
 placeholder.innerHTML = out;
-});
-
-function b2() {	
-    fetch("inventars.json")
-    .then(function(response){
-        return response.json();
-})
-.then(function(products){
-    let placeholder = document.querySelector("#data-output");
-    let out = "";
-    for(let product of products){
-    out += `
-        <tr>
-            <td>${product.id}</td>
-            <td>${product.nosaukums}</td>
-            <td>${product.tips}</td>
-            <td>${product.apakstips}</td>
-            <td>${product.skaits}</td>
-            <td>${product.daudzums}</td>
-            <td>${product.komentari}</td>
-        </tr>
-    `;
-}
-
-placeholder.innerHTML = out;
-});
-}	
+})};
